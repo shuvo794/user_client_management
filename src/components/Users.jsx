0d824acdf4252e1bf53b6c96843199325c23ec50
shuvo794 <shuvohossain794@gmail.com>
2026-06-03 14:16:01 +0600

    feat: enhance Users component to manage user addition and state updates

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,22 +1,66 @@
-import { use } from "react";
+import { use, useState } from "react";
 
 const Users = ( { userPromise } ) =>
 {
-    const users = use( userPromise );
-    console.log( 'lvjcljvclj', users );
+    const instialUsers = use( userPromise );
+    const [ users, setUsers ] = useState( instialUsers );
+    const handleAddUser = ( event ) =>
+    {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const email = form.email.value;
+
+        console.log( "Adding user:", name, email );
+
+        fetch( 'http://localhost:3000/users', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( { name, email } )
+        } )
+            .then( ( response ) => response.json() )
+            .then( ( newUser ) =>
+            {
+                console.log( "User added:", newUser );
+                const newUsers = [ ...users, newUser ];
+                setUsers( newUsers );
+            } );
+
+        // optional: reset form
+        form.reset();
+    };
+
     return (
         <div>
             <h2>Users</h2>
-            <form action="">
-                <input type="text" placeholder="Name" />
+
+            <form onSubmit={ handleAddUser }>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    required
+                />
                 <br />
-                <input type="email" placeholder="Email" />
+
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                />
                 <br />
+
                 <button type="submit">Add User</button>
             </form>
-            { users.map( user => (
+
+            { users.map( ( user ) => (
                 <div key={ user.id }>
-                    <p>{ user.name } Email : { user.email }</p>
+                    <p>
+                        { user.name } — Email: { user.email }
+                    </p>
                 </div>
             ) ) }
         </div>
